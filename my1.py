80aa3370104531083461e293de965a8ca6635c7e
polinafish <84339974+polinafish@users.noreply.github.com>
2021-08-17 00:27:31 +0200

    Update my1.py

--- my1.py
+++ my1.py
@@ -20,7 +20,27 @@ def send_welcome(message):
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
     if message.text == 'Привет':
-        bot.reply_to(message, 'Здравствуй, мой господин')
+        bot.send_message(message.from_user.id, "Как к тебе обращаться, пёс?")
+        # Готовим кнопки
+        keyboard = types.InlineKeyboardMarkup()
+
+        key_dog = types.InlineKeyboardButton(text='Пёс', callback_data='names')
+        # И добавляем кнопку на экран
+        keyboard.add(key_dog)
+        key_sun = types.InlineKeyboardButton(text='Солнышко', callback_data='names')
+        keyboard.add(key_sun)
+        key_gold = types.InlineKeyboardButton(text='Золотце', callback_data='names')
+        keyboard.add(key_gold)
+
+        bot.send_message(message.from_user.id, text='Как к тебе обращаться, пёс', reply_markup=keyboard)
+
+        # Обработчик нажатий на кнопки
+       @bot.callback_query_handler(func=lambda call: True)
+        def callback_worker(call):
+            if call.data == "names":
+                     bot.send_message(message.from_user.id, 'Да мне плевать, чертила!')
+
+
     elif message.text == '/reg':
         retryCount = 0
         bot.send_message(message.from_user.id, 'Напиши своё имя, пёс!')
@@ -48,6 +68,7 @@ def reg_age(message, retryCount):
             bot.send_message(message.from_user.id, 'Пиши цифрами, пёс!')
             retryCount = retryCount + 1
             bot.register_next_step_handler(message, reg_age, retryCount)
+            bot.send_message(message.from_user.id, 'У тебя осталось ' + str(5 - retryCount) + ' попыток, чертила!')
     if retryCount >= 5:
         bot.send_message(message.from_user.id, 'Ну ты и тупой, говорил же цифрами пиши, пес!')
         bot.clear_step_handler_by_chat_id(message.from_user.id)
@@ -56,7 +77,7 @@ def reg_age(message, retryCount):
         reg_ok(message)
 
 def reg_ok(message):
-    bot.send_message(message.from_user.id, 'Тебе ' + str(age) + ' лет? И тебя зовут' + name + ' ' + surname +
+    bot.send_message(message.from_user.id, 'Тебе ' + str(age) + ' лет? И тебя зовут ' + name + ' ' + surname +
                       '? Отвечай, пёс!')
 
 
